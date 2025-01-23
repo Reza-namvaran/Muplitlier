003b72437af26dc4d2851e33d862674210afef59
Reza-namvaran <reza.namvaran823@gmail.com>
2025-01-24 01:23:10 +0330

    [REFACTOR]: Remove Assertions

--- Modules/1x1_Mul.vhd
+++ Modules/1x1_Mul.vhd
@@ -14,12 +14,4 @@ ARCHITECTURE BEHAVIORAL OF Multiplier_1x1 IS
 BEGIN
 	-- 1x1 multiplier is just an AND gate
 	P <= A AND B;
-
-	-- Assertion for input validation
-	ASSERT (A = '0' OR A = '1') AND (B = '0' OR B = '1')
-		REPORT "Inputs must be either '0' or '1' for 1x1 multiplier" SEVERITY error;
-
-	-- Assert to check the correctness of the output
-	ASSERT (P = A * B)
-		REPORT "Product does not match expected value" SEVERITY WARNING;
 END BEHAVIORAL;
